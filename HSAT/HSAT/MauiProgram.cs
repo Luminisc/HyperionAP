6171e723c4f2b71f83af39a2dc194c7672ad36a0
Alexander Romanenko <al.a.romanenko@gmail.com>
2023-02-02 10:41:18 +0300

    Experiments

--- HSAT/HSAT/MauiProgram.cs
+++ HSAT/HSAT/MauiProgram.cs
@@ -1,18 +1,46 @@
-﻿namespace HSAT;
+﻿using Microsoft.Maui.LifecycleEvents;
+using Microsoft.UI.Windowing;
+using Microsoft.UI;
+using Windows.Graphics;
+
+namespace HSAT;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
-		return builder.Build();
-	}
+        builder.ConfigureLifecycleEvents(lifecycle =>
+        {
+            lifecycle.AddWindows(windows => windows.OnWindowCreated((window) =>
+            {
+                window.ExtendsContentIntoTitleBar = true;
+                window.Title = "HSAT";
+
+                IntPtr nativeWindowHandle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                WindowId win32WindowsId = Win32Interop.GetWindowIdFromWindow(nativeWindowHandle);
+                AppWindow winuiAppWindow = AppWindow.GetFromWindowId(win32WindowsId);
+                if (winuiAppWindow.Presenter is OverlappedPresenter p)
+                {
+                    //p.Maximize();
+                }
+                else
+                {
+                    const int width = 1280;
+                    const int height = 720;
+                    winuiAppWindow.MoveAndResize(new RectInt32(1920 / 2 - width / 2, 1080 / 2 - height / 2, width, height));
+                }
+            }));
+        });
+
+        return builder.Build();
+    }
 }
---
+++ HSAT/HSAT/Pages/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace HSAT;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		//count++;
+
+		//if (count == 1)
+		//	CounterBtn.Text = $"Clicked {count} time";
+		//else
+		//	CounterBtn.Text = $"Clicked {count} times";
+
+		//SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+
+    private void OnPanUpdated(object sender, PanUpdatedEventArgs e)
+    {
+		Console.WriteLine(e);
+		lblLabel.Text = $"{e.GestureId}: {e.StatusType} ({e.TotalX} {e.TotalY})";
+    }
+}
+
