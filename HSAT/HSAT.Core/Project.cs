a04b8399f4bc78d063dcdbb69de13e204613a571
Alexander Romanenko <al.a.romanenko@gmail.com>
2023-07-29 11:57:06 +0300

    Whoop again

--- HSAT/HSAT.Core/Project.cs
+++ HSAT/HSAT.Core/Project.cs
@@ -2,10 +2,13 @@
 {
     public class Project
     {
+        /// <summary>Name of project</summary>
         public string Name { get; set; }
 
-        public string ImagePath { get; }
-
+        /// <summary>Path to project file</summary>
         public string ProjectPath { get; }
+
+        /// <summary>Path to images data</summary>
+        public List<string> ImagePaths { get; }
     }
 }
