437cd4602f4c7175999701bd41a1aa58704b1f30
Alexander Romanenko <al.a.romanenko@gmail.com>
2025-11-02 16:30:56 +0300

    Nothing meaningful

--- HyperionAP/HyperionAP.UI.Server/Controllers/DemoController.cs
+++ HyperionAP/HyperionAP.UI.Server/Controllers/DemoController.cs
@@ -5,50 +5,46 @@ using HyperionAP.UI.Server.Services;
 using Microsoft.AspNetCore.Mvc;
 using FileInfo = HyperionAP.UI.Server.Services.FileInfo;
 
-namespace HyperionAP.UI.Server.Controllers
+namespace HyperionAP.UI.Server.Controllers;
+
+[Route("api/[controller]/[action]")]
+public class DemoController(FilesService filesService, DatasetService datasetService, IProjectRepository projectRepository, IUserRepository userRepository) : ControllerBase
 {
-    [Route("api/[controller]/[action]")]
-    public class DemoController(FilesService filesService, DatasetService datasetService, IProjectRepository projectRepository, IUserRepository userRepository) : ControllerBase
+    [HttpPost]
+    public async Task<FileInfo> Upload(IFormFile file)
     {
-        [HttpPost]
-        public async Task<FileInfo> Upload(IFormFile file)
-        {
-            var fileInfo = await filesService.AddFile(file);
-            return fileInfo;
-        }
+        var fileInfo = await filesService.AddFile(file);
+        return fileInfo;
+    }
 
-        [HttpGet]
-        public IEnumerable<FileInfo> GetFiles()
-        {
-            return filesService.GetFiles();
-        }
+    [HttpGet]
+    public IEnumerable<FileInfo> GetFiles() => filesService.GetFiles();
 
-        [HttpGet]
-        public async Task<IActionResult> GetImageBand(Guid fileId, int band)
-        {
-            var filePath = filesService.GetDatasetPath(fileId);
-            var pngBytes = datasetService.GetDemoImageBand(filePath, band);
-            return File(pngBytes, "image/png", "output.png");
-        }
+    [HttpGet]
+    public async Task<IActionResult> GetImageBand(Guid fileId, int band)
+    {
+        var filePath = filesService.GetDatasetPath(fileId);
+        var pngBytes = datasetService.GetDemoImageBand(filePath, band);
+        return File(pngBytes, "image/png", "output.png");
+    }
 
-        [HttpGet]
-        public async Task<IEnumerable<User>> GetUsers() => await userRepository.GetAllUsersAsync();
+    [HttpGet]
+    public async Task<IEnumerable<User>> GetUsers() => await userRepository.GetAllUsersAsync();
 
-        [HttpGet]
-        public async Task<IEnumerable<Project>> GetProjects() => await projectRepository.GetAllProjectsAsync();
+    [HttpGet]
+    public async Task<IEnumerable<Project>> GetProjects() => await projectRepository.GetAllProjectsAsync();
 
-        [HttpGet]
-        public async Task<User> AddUser(User user) => await userRepository.AddAsync(user);
+    [HttpGet]
+    public async Task<User> AddUser(User user) => await userRepository.AddAsync(user);
 
-        [HttpPost]
-        public async Task<Project> AddProject(string name, Guid owner)
-        {
-            // should be service logic...
-            var user = await userRepository.GetAsync(owner);
-            if (user == null)
-                throw new ArgumentException(nameof(owner));
-            var project = await projectRepository.AddAsync(new Project(name, user));
-            return project;
-        }
+    [HttpPost]
+    public async Task<Project> AddProject(string name, Guid owner)
+    {
+        // should be service logic...
+        var user = await userRepository.GetAsync(owner);
+        if (user == null)
+            throw new ArgumentException(nameof(owner));
+        var project = await projectRepository.AddAsync(new Project(name, user));
+        return project;
     }
 }
